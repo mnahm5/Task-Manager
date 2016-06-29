b6ff92e111732ed157b0eac2c39944bcd9c30161
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-06-30 09:45:34 +1000

    Added Login and Register Activities

--- app/src/main/java/com/example/mnahm5/task_manager/MainActivity.java
+++ app/src/main/java/com/example/mnahm5/task_manager/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mnahm5.task_manager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btRegisterPage = (Button) findViewById(R.id.btRegisterPage);
+        final Button btLoginPage = (Button) findViewById(R.id.btLoginPage);
+
+        btRegisterPage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(MainActivity.this, Register.class);
+                MainActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        btLoginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(MainActivity.this, Login.class);
+                MainActivity.this.startActivity(loginIntent);
+            }
+        });
     }
 }
