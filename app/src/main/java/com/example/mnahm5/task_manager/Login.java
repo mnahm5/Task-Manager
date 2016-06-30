38eaa434feb39b5e5909b785ca63ef3cd3a537e3
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-06-30 10:43:33 +1000

    Login Worked

--- app/src/main/java/com/example/mnahm5/task_manager/Login.java
+++ app/src/main/java/com/example/mnahm5/task_manager/Login.java
@@ -1,7 +1,21 @@
 package com.example.mnahm5.task_manager;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +23,63 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button btLogin = (Button) findViewById(R.id.btLogin);
+        final TextView tvRegisterLink = (TextView) findViewById(R.id.tvRegisterLink);
+
+        tvRegisterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(Login.this, Register.class);
+                Login.this.startActivity(registerIntent);
+            }
+        });
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if (success) {
+                                String fullName = jsonResponse.getString("fullName");
+                                String email = jsonResponse.getString("email");
+                                String companyName = jsonResponse.getString("companyName");
+                                Intent intent = new Intent(Login.this, Home.class);
+                                intent.putExtra("username", username);
+                                intent.putExtra("fullName", fullName);
+                                intent.putExtra("email", email);
+                                intent.putExtra("companyName", companyName);
+                                Login.this.startActivity(intent);
+                                finish();
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                                builder.setMessage("Login Failed")
+                                        .setNegativeButton("Retry",null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(Login.this);
+                queue.add(loginRequest);
+            }
+        });
     }
 }
