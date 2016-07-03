16f46836fd65e0510f0eabd721d2539bb646937a
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-07-03 11:27:29 +1000

    Added Task Details Activity

---
+++ app/src/main/java/com/example/mnahm5/task_manager/TaskDetails.java
@@ -0,0 +1,13 @@
+package com.example.mnahm5.task_manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_details);
+    }
+}
